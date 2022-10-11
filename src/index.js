aea4d048b200173f0065ccbf6fbc45ed2c190ba7
Melissa Ramirez <melissa@Melissas-MacBook-Air.local>
2022-10-11 11:40:35 -0500

    aplicado funciones isValid y Maskify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+//  Para dar opciones de mes
+for (let i= 1; i <= 12; i++){
+    let option = document.createElement('option');
+    option.value =  i;
+    option.innerText = i;
+    document.getElementById("selectMes").appendChild (option);
+}
+// Opciones  del año actual + 8 
+const yearActual = new Date ().getFullYear();
+for (let i = yearActual; i  <= yearActual + 8; i++){
+    let option = document.createElement('option'); 
+    option.value =  i;
+    option.innerText = i;
+    document.getElementById("selectYear").appendChild (option);
+}  
+
+document.getElementById("validar").addEventListener("click",function (event) {
+    event.preventDefault ();
+    let datos = document.getElementById("inputNumero").value;
+    let inputNombre = document.getElementById ("inputNombre").value;
+    if (datos === "" || inputNombre.value === ""){
+        alert("Ingresa tus datos");
+        return datos;
+    } else {
+        // Si es valida entonces alert success sino alert error
+        let creditCardNumber = document.getElementById("inputNumero").value;
+        let esValida = validator.isValid(creditCardNumber);
+        if (esValida === true){
+            alert ("Yahoo! Ya tienes tus boletos para Bad Bunny");
+        } else {
+            alert ("Verifica tus datos de nuevo");
+        }
+        //Maskify en pantalla
+        let inputNumero = document.getElementById("inputNumero");
+        let validaM = validator.maskify (inputNumero.value);
+        inputNumero.value = validaM;
+        
+    }
+})
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,51 @@
 const validator = {
-  // ...
+  
+  isValid: function (creditCardNumber) {
+     var arreglo = creditCardNumber.split("");
+     //mostrar el arreglo en la consola
+     //console.log("el arreglo es" + arreglo);
+     // convertir el string en entero con parseInt
+     const arrOfNum = arreglo.map(str => { //map retorna un array nuevo
+       return Number(str);
+     });
+     //mostrar en consola
+     //console.log("en arreglo es" + arrOfNum);
+     //variable para el lugar
+     let resultado = 0;
+     for (let i = 0; i < arrOfNum.length; i++) {
+      var numero = (arrOfNum[i]);
+      if (i % 2 === 0){
+        numero *= 2;
+        if (numero > 9){
+          numero -= 9;
+        }
+      }
+      resultado += numero;
+      //console.log (resultado);
+    }
+    if (resultado % 10 === 0){
+      //alert ("es valida");
+    } else {
+     // alert ("no es valida");
+    }
+    return resultado % 10 === 0; // AÑADIDO!!!
+  },
+  maskify : function (creditCardNumber){
+    var largo = creditCardNumber.length;
+    let resultado = "";
+    if(largo > 4) {
+      for (let i = 0; i < largo; i++) {
+        if( i < largo - 4 ) {
+            resultado += "#"
+        } else {
+          resultado += creditCardNumber[i];
+        }
+      }
+    } else {
+      return creditCardNumber;
+    }
+    return resultado;
+  }
 };
 
 export default validator;

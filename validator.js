8b11303ec8a0d3209a92cfd9bbc54d23c7b3c1ca
Melissa Ramirez <melissa@Melissas-MacBook-Air.local>
2022-10-17 09:56:17 -0500

    Updates

---
+++ validator.js
@@ -0,0 +1,48 @@
+const validator = {
+  
+  isValid: function (creditCardNumber) {
+     var arreglo = creditCardNumber.split("");
+     //mostrar el arreglo en la consola
+     //console.log("el arreglo es" + arreglo);
+     // convertir el string en entero con parseInt
+     let arrOfNum = arreglo.map (str => {
+      return Number (str);
+     });
+     let resultado = 0; 
+     for (let i = 0; i < arrOfNum.length; i++){
+      var numero = (arrOfNum[i]);
+      if ((i+1) % 2 === 0){
+        numero *= 2;
+        if (numero > 9){
+          numero -= 9;
+         // debugger;
+        } 
+      }n
+      resultado += numero;
+       //console.log (resultado);
+     }
+     if (resultado % 10 === 0){
+      return true;//alert ("es valida");
+    } else {
+      return false;// alert ("no es valida");
+    }
+  },
+  maskify : function (creditCardNumber){
+    var largo = creditCardNumber.length;
+    let resultado = "";
+    if(largo > 4) {
+      for (let i = 0; i < largo; i++) {
+        if( i < largo - 4 ) {
+            resultado += "#"
+        } else {
+          resultado += creditCardNumber[i];
+        }
+      }
+    } else {
+      return creditCardNumber;
+    }
+    return resultado;
+  }
+};
+
+export default validator;
